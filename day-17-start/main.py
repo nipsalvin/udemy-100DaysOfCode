027e9b279fe8dcaa0b6cb9b99fc15f4ecf38f4b3
nipsalvin <amwaniki.am@gmail.com>
2023-03-01 07:43:38 +0300

    DAY17 progress

--- day-17-start/main.py
+++ day-17-start/main.py
@@ -1,10 +1,25 @@
 class User:
-    def __init__(self):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.followers = 0
+        self.following = 0
         print("User is being created")
     
-user = User()
-user.name = "Alvin"
-user.age = 28
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+    
+user_1 = User("Alvin",28)
+# print(user_1.followers)
+user_2 = User("Nips", 29)
+user_1.follow(user_2)
+
+print(f'{user_1.name} has {user_1.followers} followers')
+print(f'{user_1.name} is following {user_1.following} people')
+print(f'{user_2.name} has {user_2.followers} followers')
+print(f'{user_2.name} is following {user_2.following} people')
 
 class Car:
     def __init__(self, seats, wheels):
@@ -17,3 +32,9 @@ print(f'my car has {my_car.seats} seats')
 
 his_car = Car(4, 4)   
 print(f'his car has {his_car.seats} seats')    
+
+her_car_seats = int(input("How many car seats?\n"))
+her_car_wheels = int(input("How many wheels?\n"))
+her_car = Car(her_car_seats, her_car_wheels)
+print(f'Her car has {her_car.seats} seats') 
+print(f'Her car has {her_car.wheels} wheels')
