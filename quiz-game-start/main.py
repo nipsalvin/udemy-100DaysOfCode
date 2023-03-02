7a9dda112f60be16c46f2c08350bbe79ba32a43e
nipsalvin <amwaniki.am@gmail.com>
2023-03-02 20:35:52 +0300

    DAY 17 progress

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,9 +1,15 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for i in question_data:
     questions = Question(i['text'], i['answer'])
     question_bank.append(questions)
 
-print(question_bank[0].text, question_bank[0].answer )
+# print(question_bank[0].text, question_bank[0].answer )
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+# print (quiz)
+while quiz.still_has_questions() == True:
+    quiz.next_question()
--- quiz-game-start/question_model.py
+++ quiz-game-start/question_model.py
@@ -2,3 +2,4 @@ class Question:
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
+    
