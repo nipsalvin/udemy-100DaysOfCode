0e288d7767ce107bd3594ada56687ce0da43743b
nipsalvin <amwaniki.am@gmail.com>
2023-03-05 18:40:53 +0300

    DAY-18-4 Done

--- day-18-start/day-18-4-start.py
+++ day-18-start/day-18-4-start.py
@@ -4,20 +4,27 @@ import random
 
 kamtu = t.Turtle()
 kamtu.hideturtle()
-# kamtu.shape("turtle")
+t.colormode(255)
+
+def rand_color():
+    """Generates a random RGB color to be used"""
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r,g,b)
+    return color
 
 #TODO Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180, 270]
 kamtu.speed("fastest")
 kamtu.pensize(10)
 
 for i in range(100):
+    # kamtu.color(random.choice(color))
+    # Using random RBG colors
+    kamtu.color(rand_color())
     kamtu.forward(30)
     kamtu.setheading(random.choice(directions))
-    kamtu.color(random.choice(colours))
-
-
 
 screen = Screen()
 screen.exitonclick()
