27609a860cc614280777187abdf883d6bdfdde8a
nipsalvin <amwaniki.am@gmail.com>
2023-03-06 07:24:23 +0300

    Hirst painting progress

--- day-18-start/hirst_painting/main.py
+++ day-18-start/hirst_painting/main.py
@@ -1,8 +1,15 @@
-import colorgram
+# import colorgram
 
-color = colorgram.extract('hirst.jpg', 7)
+# rgb_colors = []
+# colors = colorgram.extract('hirst.jpg', 7)
 
-first_color = color[0]
-rgb = first_color.rgb
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
 
-print(rgb)
+# print(rgb_colors)
+color_list = [(199, 175, 117), 
+              (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 224, 227)]
