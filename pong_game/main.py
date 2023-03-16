745f4e6582bfe51992c73d5e582c84b1708c8593
nipsalvin <amwaniki.am@gmail.com>
2023-03-16 21:13:27 +0300

    DAY-22 Progress

--- pong_game/main.py
+++ pong_game/main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 import random
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 # TODO 1: Create screen
@@ -19,6 +20,8 @@ l_paddle = Paddle(-350, 0)
 # TODO 3.1: Create ball
 ball = Ball()
 
+score = Scoreboard()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, 'Up')
 screen.onkey(r_paddle.go_down, 'Down')
@@ -27,14 +30,26 @@ screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.05)
+    time.sleep(0.03)
     screen.update()
     ball.move()
 
-    # TODO 5: Detect collision with wall
-    if ball.ycor() > 290 or ball.ycor() < -290:
-        ball.bounce()
+    # TODO 4.1: Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
     
+    # TODO 5.1: Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    
+    # TODO 6: Detect if ball is out of bounds
+    if ball.xcor() > 380 :
+        ball.reset_position()
+    
+    if ball.xcor() < -380:
+        ball.reset_position()
+    
+
 
 
 screen.exitonclick()
