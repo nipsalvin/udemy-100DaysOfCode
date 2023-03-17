7d7600a0185416086bbb758c9c16579d3fccb142
nipsalvin <amwaniki.am@gmail.com>
2023-03-17 07:14:50 +0300

    Day 22 complete

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -10,6 +10,7 @@ class Ball(Turtle):
         self.goto(0,0)
         self.x_move = 5
         self.y_move = 5
+        self.move_speed = 0.1
     
     # TODO 3.2: Create ball
     def move(self):
@@ -26,8 +27,12 @@ class Ball(Turtle):
     def bounce_x(self):
         """Detects collision with paddle and changes direction"""
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0,0)
+        self.move_speed = 0.1
         self.bounce_x()
         
+        
+
