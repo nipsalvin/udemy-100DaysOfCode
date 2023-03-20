b66bdcac0d227817f95eaaf1f3c4cbb78c80039e
nipsalvin <amwaniki.am@gmail.com>
2023-03-20 07:47:13 +0300

    DAY 23 initial commit

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -8,7 +8,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.05)
     screen.update()
+
+
