f8faa9aa798e6e2a22b2b81aabe3a63e1af7a72d
nipsalvin <amwaniki.am@gmail.com>
2023-03-21 19:50:46 +0300

    snake game update

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,5 +1,4 @@
 from turtle import Turtle
-from food import Food
 
 ALIGNMENT = 'center'
 FONT = ("courier", 24, "normal")
@@ -9,26 +8,24 @@ class Scoreboard(Turtle):
     def __init__(self):
         """This creates the scoreboard as a turtle object"""
         super().__init__()
-        self.hideturtle()
-        self.penup()
         self.score = 0
-        self.highscore = 0
         self.color("white")
-        self.goto(0, 265)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
             
-    def count(self):
-        """Adds 1 to the score anytime it is called"""
-        self.score += 1
-        if self.score >= self.highscore:
-            self.highscore = self.score
-        self.clear()
-        self.write(f"Score: {self.score}, HighScore: {self.highscore}", align=ALIGNMENT, font=FONT)
-        
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
     def game_over(self):
-        """Prints GAME OVER once it is called"""
-        self.goto(0,0)
-        self.write("GAME OVER", align="center", font= FONT )
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
     
 
             
