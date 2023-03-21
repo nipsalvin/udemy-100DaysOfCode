baec8297903b58e6473b6f4e0ae756db45837f14
nipsalvin <amwaniki.am@gmail.com>
2023-03-21 09:22:35 +0300

    Day 23 Complete

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -2,25 +2,26 @@ from turtle import Turtle
 
 FONT = ("Courier", 24, "normal")
 
+# TODO 3: Create and update scoreboard
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.color("black")
-        self.penup()
         self.hideturtle()
-        self.score = 0
+        self.penup()
+        self.goto(-280, 250)
+        self.level = 1
         self.update()
     
     def update(self):
         self.clear()
-        self.goto(-250, 250)
-        self.write(self.score, font=FONT)
+        self.write(f"Level: {self.level}", font=FONT)
     
     def next_level(self):
-        self.score += 1
+        self.level += 1
         self.update()
     
     def game_over(self):
-        self.goto(-40,0)
+        self.goto(0,0)
         self.penup()
-        self.write("GAME OVER", font=FONT)
+        self.write("GAME OVER",align='center' , font=FONT)
