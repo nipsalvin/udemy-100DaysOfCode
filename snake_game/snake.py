bf5597c5859d02ac846e5de47a3056def3dc3db4
nipsalvin <amwaniki.am@gmail.com>
2023-03-22 07:44:57 +0300

    DAY-24 inistial commit

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -26,15 +26,23 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
-    def extend(self):
-        self.add_segment(self.segments[-1].position())
-
     def move_snake(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+    
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
         
     def up(self):
         if self.head.heading() != DOWN:
