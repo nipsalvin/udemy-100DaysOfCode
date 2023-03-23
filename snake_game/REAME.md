33e93c049ee3971bd9224c3684fd8c0e314ce7e4
nipsalvin <amwaniki.am@gmail.com>
2023-03-23 20:48:04 +0300

    DAY-24 progress

---
+++ snake_game/REAME.md
@@ -0,0 +1,9 @@
+# This is a snake game 
+
+It is run by running the `main.py` file.
+The snake starts as 3 segments.
+The game detects collision of the snake with the food and adds a segment to the last positon
+After collision the food also relocates to a random position on the screen.
+The game also detecs collision with the walls and resets the game.
+The highscore is saved in a txt file called `data.txt`.
+This is updated whenever the highscore is beaten and scoreboard is updated.
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
         """This creates the scoreboard as a turtle object"""
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -19,12 +20,14 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         """This function updates the score board"""
         self.clear()
-        self.write(f"Score: {self.score} HighScore: {self.highscore}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} HighScore: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         """This resets the score to 0 and calls update_scoreboard()"""
-        if self.score > self.highscore:
-            self.highscore = self.score
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode= 'w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
