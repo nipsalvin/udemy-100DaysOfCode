f843263eba2f538ac13de9f153ecd18aeca25504
nipsalvin <amwaniki.am@gmail.com>
2023-04-04 07:57:59 +0300

    day 25 progress

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -8,14 +8,25 @@ screen.title("U.S States Game")
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
 
-game_on = True
-
-correct_answers = []
-while game_on:
-    answer_state = screen.textinput("Guess the state", "What's another state's name?").title()
+guessed_state = []
+score = 0
+while len(guessed_state) < 50:
+    t = turtle.Turtle()
+    t.penup()
+    t.hideturtle()
+    answer_state = screen.textinput(f"{len(guessed_state)}/50 states correct", "What's another state's name?").title()
     data = pandas.read_csv("50_states.csv")
-    answer_state in data["state"]
-    # print(correct_answers)
+    for check in data.state:
+        if answer_state == check and answer_state not in guessed_state:
+            guessed_state.append(answer_state)
+            score += 1
+            """Get the row of data"""
+            state_data = data[data.state == answer_state]
+            t.goto(int(state_data.x), int(state_data.y))
+            # print(score)
+            # print(correct_answers)
+            t.write(answer_state)
+
     # print(answer_state)
     # print(state)
 screen.exitonclick()
