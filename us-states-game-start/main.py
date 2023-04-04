47299464ad2fbde22aaa7858c5d50a2758bc6823
nipsalvin <amwaniki.am@gmail.com>
2023-04-04 07:00:26 +0300

    day 25 progress

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -14,7 +14,8 @@ correct_answers = []
 while game_on:
     answer_state = screen.textinput("Guess the state", "What's another state's name?").title()
     data = pandas.read_csv("50_states.csv")
-    
+    answer_state in data["state"]
+    # print(correct_answers)
     # print(answer_state)
     # print(state)
 screen.exitonclick()
