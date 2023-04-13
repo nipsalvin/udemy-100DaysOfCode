913fdf5371bfe6b5085c843c358c21f33fff1d2e
nipsalvin <amwaniki.am@gmail.com>
2023-04-13 03:57:03 +0300

    day 26 progress

---
+++ day-26/dictionary_comprehension/main.py
@@ -0,0 +1,8 @@
+import random
+names = ['Alex', 'Beth', 'Carol', 'Dave', 'Elanor', 'Freddie']
+
+student_scores = {student:random.randint(1,100) for student in names}
+print (student_scores)
+
+passed_students = {student:score for (student, score) in student_scores.items() if score >= 60}
+print(passed_students)
