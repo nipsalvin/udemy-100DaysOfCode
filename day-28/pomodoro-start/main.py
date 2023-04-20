032b9eb5cb141b702d5b6940c897044136fb4ae9
nipsalvin <amwaniki.am@gmail.com>
2023-04-20 06:32:43 +0300

    day 28 initial commit

---
+++ day-28/pomodoro-start/main.py
@@ -0,0 +1,33 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+canvas = Canvas(width=220, height=240, bg=YELLOW, highlightthickness= 0)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(103, 112, image = tomato_img)
+canvas.create_text(103, 130,text='00:00' ,fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.pack()
+
+
+window.mainloop()
+
+
