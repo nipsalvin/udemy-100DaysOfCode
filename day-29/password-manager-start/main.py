b329126771b9a8f3cadc225cd6fa6fddf22d31a6
nipsalvin <amwaniki.am@gmail.com>
2023-04-23 17:41:04 +0300

    day 29 initial

---
+++ day-29/password-manager-start/main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file ='logo.png')
+canvas.create_image(100, 100, image = lock_img)
+canvas.grid(row=1, column=1)
+
+window.mainloop()
