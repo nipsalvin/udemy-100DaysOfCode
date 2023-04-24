5656f5a3a48a1c7dba075d4aa648c7cf63a9f17b
nipsalvin <amwaniki.am@gmail.com>
2023-04-24 20:19:41 +0300

    day-29 progress

--- day-29/password-manager-start/main.py
+++ day-29/password-manager-start/main.py
@@ -3,6 +3,25 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    """
+    This takes the data inputted in the entries and saves them to a `data.txt` file
+    """
+    ### --- Getting the data --- ###
+    website_data = website_entry.get()
+    email_data = email_entry.get()
+    password_data = password_entry.get()
+    ### --- --- ###
+
+    with open("data.txt", mode='a') as data_file:
+        data_file.write(f'{website_data} | {email_data} | {password_data} \n')
+
+    ### --- Deleting the data --- ###
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+    ### --- --- ###
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
@@ -22,19 +41,21 @@ password.grid(row=3, column=0)
 email = Label(text='Email/UserName')
 email.grid(row=2, column=0)
 
-website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus() #Starts the GUI with the cursor here by default 
 
-email_input = Entry(width=35)
-email_input.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, 'amwaniki.am@gmail.com')
 
-password_input = Entry(width=17)
-password_input.grid(row=3, column=1)
+password_entry = Entry(width=17)
+password_entry.grid(row=3, column=1)
 
 generate = Button(text='Generate Password')
 generate.grid(row=3,column=2)
 
-add_pass = Button(text='Add Password', width=30)
+add_pass = Button(text='Add Password', width=30, command=save)
 add_pass.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
