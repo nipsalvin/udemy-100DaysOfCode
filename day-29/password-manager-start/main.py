27eb5b5576e0c3a66aa9d9fb14d92ea8c39756db
nipsalvin <amwaniki.am@gmail.com>
2023-04-25 07:54:51 +0300

    day 29 progress

--- day-29/password-manager-start/main.py
+++ day-29/password-manager-start/main.py
@@ -1,28 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
-from random import *
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-password_list = []
+    password_list = []
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
 
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
 
-random.shuffle(password_list)
+    pass_gen = random.shuffle(password_list)
+    password_entry.insert(0, f'{pass_gen}')
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -82,7 +84,7 @@ email_entry.insert(0, 'amwaniki.am@gmail.com')
 password_entry = Entry(width=17)
 password_entry.grid(row=3, column=1)
 
-generate = Button(text='Generate Password')
+generate = Button(text='Generate Password', command=generate_password)
 generate.grid(row=3,column=2)
 
 add_pass = Button(text='Add Password', width=30, command=save)
