9ce30ad7de9540f5b3923a3bcdba59ea0c69bdd1
nipsalvin <amwaniki.am@gmail.com>
2023-05-08 20:07:58 +0300

    day 31 progress

--- day-31/main.py
+++ day-31/main.py
@@ -6,40 +6,58 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv('./data/french_words.csv')
 to_learn = data.to_dict(orient='records')
+current_card = {}
+data_frame = pandas.DataFrame(current_card)
+data_frame.to_csv('./data/known_words.csv')
 
-# ---------------------------- SEARCH BUTTON ------------------------------- #
+# ---------------------------- PICK A CARD ------------------------------- #
 
-def next_word():
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     # print(data)
-    canvas.itemconfig(card_title, text='French')
-    canvas.itemconfig(card_word, text=current_card['French'])
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(card_word, text=current_card['French'],fill='black')
+    canvas.itemconfig(card_background, image=front_img)
+    flip_timer=window.after(3000, flip_card)
 
-# -------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------ #
+
+# ---------------------------- PICK A CARD ------------------------------- #
+
+def flip_card():
+    canvas.itemconfig(card_title, text='English', fill='White')
+    canvas.itemconfig(card_word, text=current_card['English'], fill='White')
+    canvas.itemconfig(card_background, image=back_img)
+
+# ------------------------------------------------------------------------ #
+
 window = Tk()
 window.title('Flashy')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=526)
-back_img = PhotoImage(file='./images/card_back.png')
-front_img = PhotoImage(file='./images/card_front.png')
+flip_timer = window.after(3000, flip_card)
 
-canvas.create_image(400, 263, image = front_img) #set image at center of canvas (1/2X, 1/2Y)
+canvas = Canvas(width=800, height=526)
+front_img = PhotoImage(file='./images/card_front.png')
+back_img = PhotoImage(file='./images/card_back.png')
+
+card_background= canvas.create_image(400, 263, image = front_img) #set image at center of canvas (1/2X, 1/2Y)
 canvas.config(bg= BACKGROUND_COLOR, highlightthickness=0)
 card_title = canvas.create_text(400, 100, text='', font=('ariel', 20, 'italic'))
 card_word = canvas.create_text(400, 263, text='', font=('TimesNewRoman', 30, 'bold'))
 canvas.grid(row=0, column=0, columnspan=2)
 
 wrong_img = PhotoImage(file='./images/wrong.png')
-wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_word)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 right_img = PhotoImage(file='./images/right.png')
-right_button = Button(image=right_img, highlightthickness=0, command=next_word)
+right_button = Button(image=right_img, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 
 
-next_word()
-
+next_card()
 
 window.mainloop()
