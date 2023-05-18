76621a2f9d44a3de7c4a47846c3068ab72564fd6
nipsalvin <amwaniki.am@gmail.com>
2023-05-18 20:38:47 +0300

    day 32 progress

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -7,7 +7,6 @@ import random
 # 1. Update the birthdays.csv with your friends & family's details. 
 # HINT: Make sure one of the entries matches today's date for testing purposes. 
 
-
 # 2. Check if today matches a birthday in the birthdays.csv
 # HINT 1: Only the month and day matter. 
 # HINT 2: You could create a dictionary from birthdays.csv that looks like this:
@@ -34,6 +33,7 @@ today = dt.datetime.now()
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT: https://www.w3schools.com/python/ref_string_replace.asp
+PLACEHOLDER = '[name]'
 
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
