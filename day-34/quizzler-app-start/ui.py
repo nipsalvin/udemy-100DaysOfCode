d53864c471f77a177a297915b25a99231382475e
nipsalvin <amwaniki.am@gmail.com>
2023-05-25 07:29:26 +0300

    day 34 progress

--- day-34/quizzler-app-start/ui.py
+++ day-34/quizzler-app-start/ui.py
@@ -1,31 +1,45 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface():
 
-    def __init__(self):
+    def __init__(self, quiz_brain:QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
+        self.score_label = Label(text='Score:', 
+                           font=('ariel', 20, 'italic'), 
+                           bg=THEME_COLOR, fg='white')
+        self.score_label.grid(row=0, column=1)
+
         self.canvas = Canvas(width=300, height=250)
-        self.canvas.grid(row=1, column=0, columnspan=2)
-
-        self.score = Label(text='Score:', font=('ariel', 20, 'italic'), bg=THEME_COLOR, fg='white')
-        self.score.grid(row=0, column=1)
-
-        self.question_text = self.canvas.create_text(400,100, text='Question Text', font=('ariel', 20, 'italic'))
+        self.question_text = self.canvas.create_text(150,125, 
+                                                     width=280,
+                                                     text='Question Text', 
+                                                     font=('ariel', 20, 'italic'), 
+                                                     fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         
-        self.true_img = PhotoImage(file='day-34/quizzler-app-start/images/true.png')
-        self.true_button=Button(image=self.true_img, highlightthickness=0)
+        true_img = PhotoImage(file='day-34/quizzler-app-start/images/true.png')
+        self.true_button=Button(image=true_img, highlightthickness=0)
         self.true_button.grid(row=2, column=0)
 
-        self.false_img = PhotoImage(file='day-34/quizzler-app-start/images/false.png')
-        self.false_button = Button(image=self.false_img, highlightthickness=0)
+        false_img = PhotoImage(file='day-34/quizzler-app-start/images/false.png')
+        self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
-    
+        
+        self.get_question()
+
+        self.window.mainloop()
+
+    def get_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
         
 
 
-        self.window.mainloop()
