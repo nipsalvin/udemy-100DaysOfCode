24bfb9bca58180dd10747b6dd6fcef5d4ca80044
Sir Mwaniki Alvin IV <68748209+nipsalvin@users.noreply.github.com>
2023-05-28 19:06:06 +0300

    Delete Test directory

--- Test/test.py
+++
@@ -1,9 +0,0 @@
-import random
-names = ['Alex', 'Beth', 'Carol', 'Dave', 'Elanor', 'Freddie']
-
-student_scores = {student:random.randint(1,100) for student in names}
-print (f'Student Scores : {student_scores}')
-
-# passed_students = {{new_key:new_value} for (key:value) in dict.items() if test}
-passed_students = {key:value for (key,value) in student_scores.items() if value > 50}
-print(f'Passed Students : {passed_students}')
