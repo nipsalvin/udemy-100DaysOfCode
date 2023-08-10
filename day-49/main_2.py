2d0f44a8976443f3d9cafd6a00e871dabd954f82
nipsalvin <amwaniki.am@gmail.com>
2023-08-10 20:09:13 +0300

    day 49 complete

--- day-49/main_2.py
+++ day-49/main_2.py
@@ -33,7 +33,7 @@ password_field.send_keys(Keys.ENTER)
 time.sleep(5)
 all_jobs = driver.find_elements(By.CSS_SELECTOR, '.job-card-container--clickable')
 
-for job in all_jobs:
+for job in all_jobs: ##FIXME Crapping out on the second run of the loop
     print(f'{job.text} called')
     job.click()
     time.sleep(5)
@@ -43,21 +43,28 @@ for job in all_jobs:
         button = driver.find_element(By.CSS_SELECTOR, '.jobs-s-apply button')
         button.click()
         time.sleep(5)
+        
+        labels = driver.find_elements(By.CLASS_NAME, 'artdeco-text-input--input' )
 
-        phone = driver.find_element(By.CLASS_NAME, 'artdeco-text-input--input' )
+        for i in labels:
+            if i.accessible_name == 'Mobile phone number':
+                phone = i
+
         if phone.text == "":
             phone.send_keys(PHONE_NUMBER)
 
         submit = driver.find_element(By.CSS_SELECTOR, 'footer button')
         submit.click()
 
-         #If the submit_button is a "Next" button, then this is a multi-step application, so skip.
-        if submit.get_attribute("data-control-name") == "continue_unify":
+        #If the submit_button is a "Next" button, then this is a multi-step application, so skip.
+        # if submit.get_attribute("data-control-name") == "continue_unify":
+        if submit.accessible_name == 'Continue to next step':
+            import ipdb;ipdb.set_trace()
             close_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
             close_button.click()
             time.sleep(3)
 
-            discard_button = driver.find_elements(By.CLASS_NAME, 'artdeco-modal__confirm-dialog-btn')[1]
+            discard_button = driver.find_elements(By.CLASS_NAME, 'artdeco-modal__confirm-dialog-btn')[0]
             discard_button.click()
             print("Complex application, skipped.")
             continue
