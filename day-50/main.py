0bc5920f1f643b1a55ecf35a0fc6376759b6bac5
nipsalvin <amwaniki.am@gmail.com>
2023-08-17 07:29:12 +0300

    day 50 progress

--- day-50/main.py
+++ day-50/main.py
@@ -18,8 +18,10 @@ log_in_text = driver.find_element(By.LINK_TEXT, 'Log in')
 log_in_text.click()
 sleep(5)
 # log_in_with_number = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button')
-log_in_with_fb = driver.find_element(By.XPATH, '//*[@id="email"]')
+log_in_with_fb = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]/div/div')
 
+#Returns a list of window handles or identifiers for all the currently open browser windows or tabs
+driver.window_handles
 
 
 driver.quit()
