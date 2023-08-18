6ae81bcc2db1792d8aa23729c0c314714eae595c
nipsalvin <amwaniki.am@gmail.com>
2023-08-18 07:49:09 +0300

    day 50 progress

--- day-50/main.py
+++ day-50/main.py
@@ -13,15 +13,45 @@ URL = 'https://tinder.com/app/recs'
 driver = webdriver.Firefox()
 driver.get(URL)
 
-sleep(5)
+sleep(10)
 log_in_text = driver.find_element(By.LINK_TEXT, 'Log in')
 log_in_text.click()
-sleep(5)
-# log_in_with_number = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button')
-log_in_with_fb = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]/div/div')
 
-#Returns a list of window handles or identifiers for all the currently open browser windows or tabs
-driver.window_handles
+sleep(5)
+log_in_with_fb = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]/div/div')
+log_in_with_fb.click()
+
+#Store browser windows as variables
+tinder_window = driver.window_handles[0]
+fb_window =  driver.window_handles[1]
+
+#Switch to pop-up login window
+driver.switch_to.window(fb_window)
+driver.maximize_window()
+email = driver.find_element(By.NAME, 'email')
+email.send_keys('')
+password =  driver.find_element(By.NAME, 'pass')
+password.send_keys('')
+fb_login_button = driver.find_element(By.ID, 'loginbutton')
+fb_login_button.click()
+
+#Switch back to original page
+driver.switch_to.window(tinder_window)
+#Allow location
+allow_location = driver.find_element(By.CLASS_NAME, 'l17p5q9z')
+allow_location.click()
+#Finding all `allow buttons`
+allows = driver.find_elements(By.CLASS_NAME, 'l17p5q9z')
+#Accepting cookies
+cookies = allows[2]
+cookies.click()
+#Disabling Notifications
+disable_notifications = allows[1]
+disable_notifications.click()
+
+
+
+
 
 
 driver.quit()
