fde83e8e4ce525d848f6862d160ccd73de9fa18a
nipsalvin <amwaniki.am@gmail.com>
2023-08-18 21:17:27 +0300

    day-50 complete

--- day-50/main.py
+++ day-50/main.py
@@ -4,16 +4,27 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 
 load_dotenv()
 
-PHONE_NUMBER = os.getenv('TWILIO_VERIFIED_NUMBER')
+EMAIL = os.getenv('MY_EMAIL_2')
+FB_PASSWORD = os.getenv('FACEBOOK_PASSWORD')
 URL = 'https://tinder.com/app/recs'
 
-driver = webdriver.Firefox()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--use-fake-ui-for-media-stream')  # Suppress location prompt
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
 sleep(10)
+#Allow cookies
+pop_up = driver.find_elements(By.CLASS_NAME, 'l17p5q9z')
+allow_cookies = pop_up[3]
+allow_cookies.click()
+
 log_in_text = driver.find_element(By.LINK_TEXT, 'Log in')
 log_in_text.click()
 
@@ -26,32 +37,48 @@ tinder_window = driver.window_handles[0]
 fb_window =  driver.window_handles[1]
 
 #Switch to pop-up login window
+sleep(10)
 driver.switch_to.window(fb_window)
 driver.maximize_window()
 email = driver.find_element(By.NAME, 'email')
-email.send_keys('')
+email.send_keys(EMAIL)
 password =  driver.find_element(By.NAME, 'pass')
-password.send_keys('')
+password.send_keys(FB_PASSWORD)
 fb_login_button = driver.find_element(By.ID, 'loginbutton')
 fb_login_button.click()
 
 #Switch back to original page
 driver.switch_to.window(tinder_window)
 #Allow location
+sleep(10)
 allow_location = driver.find_element(By.CLASS_NAME, 'l17p5q9z')
 allow_location.click()
-#Finding all `allow buttons`
-allows = driver.find_elements(By.CLASS_NAME, 'l17p5q9z')
-#Accepting cookies
-cookies = allows[2]
-cookies.click()
-#Disabling Notifications
-disable_notifications = allows[1]
+
+sleep(5)
+#Accepting cookies and Disabling Notifications
+disable_notifications = driver.find_elements(By.CLASS_NAME, 'l17p5q9z')
+disable_notifications = disable_notifications[1]
 disable_notifications.click()
 
+# TODO: Start swiping
+for n in range(50):
+    sleep(2)
+    try:
+        dislike_button = driver.find_element(By.XPATH,'//*[@id="q1298270057"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[2]')
+        like_button = driver.find_element(By.XPATH,'//*[@id="q1298270057"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]')
+        if n%2 == 0:
+            like_button.click()
+        else:
+            dislike_button.click()
 
-
-
+    #Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element(By.CSS_SELECTOR, ".itsAMatch a")
+            match_popup.click()
+        #Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        except NoSuchElementException:
+            sleep(2)
 
 
 driver.quit()
