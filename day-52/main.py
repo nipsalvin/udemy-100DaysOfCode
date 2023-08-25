926d5a61c51569d18c72716fed5b920a9879f972
nipsalvin <amwaniki.am@gmail.com>
2023-08-25 20:42:29 +0300

    day-52 complete

--- day-52/main.py
+++ day-52/main.py
@@ -4,6 +4,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 
 load_dotenv()
 
@@ -16,7 +17,7 @@ URL = 'https://www.instagram.com/accounts/login/'
 
 class InstaFollower:
     def __init__(self) -> None:
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.maximize_window()
 
         
@@ -32,7 +33,7 @@ class InstaFollower:
         password.send_keys(PASSWORD)
         sleep(2)
         password.send_keys(Keys.ENTER)
-        sleep(5)
+        sleep(20)
         # self.driver.quit()
         not_now = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div')
         not_now.click()
@@ -43,16 +44,29 @@ class InstaFollower:
 
     def find_followers(self):
         '''Finds followers in the target IG account'''
+        import ipdb;ipdb.set_trace()
         self.driver.get(TARGET_ACCOUNT)
         sleep(5)
-        followers = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a')
-        followers.click()
+        Target_account_followers = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a')
+        Target_account_followers.click()
+        # scrolling
+        scrollable = self.driver.find_element(By.CLASS_NAME, "_aano")
+        for i in range(10):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable)
+            sleep(3)
 
 
 
-    def follow():
+    def follow(self):
         '''Follows the followers in the target IG account'''
-        pass
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, "li button")
+        for follower in all_buttons:
+            try:
+                follower.click()
+                sleep(2)
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element(By.XPATH, '//*[@id="mount_0_0_pI"]/div/div/div[3]/div/div[2]/div[1]/div/div[2]/div/div/div/div/div/div/button[2]')
+                cancel_button.click()
 
 
 insta_bot = InstaFollower()
