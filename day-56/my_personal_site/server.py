20a227946a8b18b3948aca9d409cc319ae8b9caf
nipsalvin <amwaniki.am@gmail.com>
2023-09-04 19:25:05 +0300

    day-56 progress

--- day-56/my_personal_site/server.py
+++ day-56/my_personal_site/server.py
@@ -3,8 +3,8 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def hello(name = None):
+    return render_template('index.html', name = name)
 
 if __name__ == '__main__':
     app.run(debug=True)
