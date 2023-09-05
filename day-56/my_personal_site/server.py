a1be891fa82f8adaeaadb6053699a9c06312a123
nipsalvin <amwaniki.am@gmail.com>
2023-09-05 20:44:33 +0300

    personal site complete

--- day-56/my_personal_site/server.py
+++ day-56/my_personal_site/server.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello(name = None):
-    return render_template('index.html', name = name)
+def hello():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
