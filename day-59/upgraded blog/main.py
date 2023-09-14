2074e846f2d6cb4cbc21a02a0913eb5b467b86f6
nipsalvin <amwaniki.am@gmail.com>
2023-09-14 07:45:37 +0300

    day-59 progress

--- day-59/upgraded blog/main.py
+++ day-59/upgraded blog/main.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
+import requests
+from post import Post
+
 app = Flask(__name__)
+
+response = requests.get('https://api.npoint.io/7770e7eff9765a42992b')
+all_posts = response.json()
+
+post_objects = []
+
+for post in all_posts:
+    post_obj = Post(post['id'], post['title'], post['subtitle'], post['body'], post['date'], post['author'])
+    post_objects.append(post_obj)
 
 @app.route('/')
 def get_all_posts():
-    return render_template('index.html')
+    return render_template('index.html', all_posts = all_posts)
 
 @app.route('/about')
 def about():
@@ -12,6 +24,14 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/posts/<int:id>')
+def show_post(id):
+    requested_post = None
+    for post in post_objects:
+        if post.id == id:
+            requested_post = post
+    return render_template('post.html', post=requested_post)
 
 if __name__ == '__main__':
     app.run(debug=True)
