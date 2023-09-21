0ff4911f6a18ab81eece7b0d1213b414ac67fc59
nipsalvin <amwaniki.am@gmail.com>
2023-09-21 20:41:00 +0300

    day-61 progress

--- day-61/day-61-starting-files-flask-secrets/main.py
+++ day-61/day-61-starting-files-flask-secrets/main.py
@@ -1,25 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, PasswordField
-from wtforms.validators import DataRequired
-
-class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    
+from wtforms import StringField, PasswordField
+import os    
 
 
 app = Flask(__name__)
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
+
+class LoginForm(FlaskForm):
+    name = StringField('Name')
+    email = StringField('Email')
+    password = PasswordField('Password')
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
