8ffd75350224b796dd83e174e36fe7c67e33f1af
nipsalvin <amwaniki.am@gmail.com>
2023-09-30 18:47:13 +0300

    day-63 progress

--- day-63/SQLAlchemy/main.py
+++ day-63/SQLAlchemy/main.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 # Initialise the app with the extension
 db.init_app(app)
 
-class Books(db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     author = db.Column(db.String(250), nullable=False)
@@ -21,13 +21,51 @@ class Books(db.Model):
     def __repr__(self):
         return f'<Book {self.title}>'
 
-
 # Create table schema in the database. Requires application context.
 with app.app_context():
-    new_book = Books(title= "Outwitting the Devil", author= "Napoleon Hill", rating= 9.3)
-    db.session.add(new_book)
+    db.create_all()
+
+# Create table schema in the database. Requires application context.
+# with app.app_context():
+#     new_book = Book(title= "TGR", author= "Napoleon Hill", rating= 9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
+
+#Read All Records
+with app.app_context():
+    # import ipdb;ipdb.set_trace()
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+    print(all_books.all())
+
+#Read A Particular Record By Query
+with app.app_context():
+    # import ipdb;ipdb.set_trace()
+    book = db.session.execute(db.select(Book).where(Book.title == 'TGR'))
+    book = book.scalar()
+    print(book)
+
+#Update A Particular Record By Query
+# with app.app_context():
+#     # import ipdb;ipdb.set_trace()
+#     update = db.session.execute(db.select(Book).where(Book.author == 'Naps Hill')).scalar()
+#     update.author = 'Napoleon Hill'
+#     db.session.commit()
+
+#Update A Particular Record By PRIMARY KEY
+with app.app_context():
+    # import ipdb;ipdb.set_trace()
+    update = db.session.execute(db.select(Book).where(Book.id == 1)).scalar()
+    update.author = 'James Clear'
+    update.title = 'Atomic Habits'
     db.session.commit()
 
+#Delete a particular Record
+with app.app_context():
+    deletion = db.session.execute(db.select(Book).where(Book.id == 3)).scalar()
+    # or book_to_delete = db.get_or_404(Book, book_id) ## this function gets object by id
+    db.session.delete(deletion)
+    db.session.commit()
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
