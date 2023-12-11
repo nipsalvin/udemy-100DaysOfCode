79fd66f9e80ec9261664bb3d7380e1ed652e853c
nipsalvin <amwaniki.am@gmail.com>
2023-12-11 20:03:24 +0300

    Minor tweak in selenium project

--- day-50/main.py
+++ day-50/main.py
@@ -33,6 +33,7 @@ sleep(5)
 log_in_with_fb = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]/div/div')
 log_in_with_fb.click()
 
+sleep(15)
 #Store browser windows as variables
 tinder_window = driver.window_handles[0]
 fb_window =  driver.window_handles[1]
@@ -50,10 +51,10 @@ fb_login_button = driver.find_element(By.ID, 'loginbutton')
 fb_login_button.click()
 
 #Switch back to original page
-driver.switch_to.window(tinder_window) 
+driver.switch_to.window(tinder_window)
 #Allow location
 sleep(20)
-allow_location = driver.find_element(By.CLASS_NAME, 'l17p5q9z')
+allow_location = driver.find_elements(By.CLASS_NAME, 'l17p5q9z')[1]
 allow_location.click()
 
 sleep(10)
@@ -62,12 +63,12 @@ disable_notifications = driver.find_elements(By.CLASS_NAME, 'l17p5q9z')
 disable_notifications = disable_notifications[1]
 disable_notifications.click()
 sleep(10)
-# TODO: Start swiping
+# TODO: Start swiping #FIXME
 dislike_button = driver.find_element(By.XPATH,'//*[@id="q1298270057"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div/div[2]/button')
 like_button = driver.find_element(By.XPATH,'//*[@id="q1298270057"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div/div[4]/button')
 for n in range(50):
     sleep(2)
-    try:   
+    try:
         if n%2 == 0:
             like_button.click()
             print('Swipped Right')
@@ -86,4 +87,3 @@ for n in range(50):
 
 
 driver.quit()
-
