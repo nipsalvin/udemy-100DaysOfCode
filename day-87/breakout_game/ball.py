6d129b6de5c585bc24dede0eeef1c5da7953434b
nipsalvin <amwaniki.am@gmail.com>
2024-01-13 14:25:02 +0300

    `Break-Out Game` designed using `Turtle`

--- day-87/breakout_game/ball.py
+++ day-87/breakout_game/ball.py
@@ -5,4 +5,28 @@ class Ball(Turtle):
         super().__init__(shape='circle')
         self.color('white')
         self.penup()
+        self.goto(x=0, y=0)
+        self.x_move = 5
+        self.y_move = 5
         self.move_speed = 0.1
+    
+    def move(self):
+        '''Gets current coordinates of the ball and moves it according to the y_move variable'''
+        x = self.xcor ()+ self.x_move
+        y = self.ycor() + self.y_move
+        self.goto(x=x, y=y)
+    
+    def bounce_y(self):
+        '''This function changes vertical direrection of the ball when called'''
+        self.y_move *= -1
+        self.move_speed *= 0.95
+
+    def bounce_x(self):
+        '''This function changes the lateral direction of the ball when called'''
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(x=0,y=-280)
+        self.move_speed = 0.1
+        self.bounce_y()
+
--- day-87/breakout_game/main.py
+++ day-87/breakout_game/main.py
@@ -1,4 +1,54 @@
 from turtle import Screen
-import time
 from ball import Ball
 from paddle import Paddle
+from bricks import Bricks
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor('black')
+screen.title(titlestring='Break Out Game')
+screen.setup(width=600, height=800)
+screen.tracer(0)
+
+paddle = Paddle(x=0, y=-300)
+ball = Ball()
+scores = Scoreboard()
+bricks = Bricks()
+bricks.create_bricks()
+
+screen.listen()
+screen.onkey(paddle.go_left, 'Left')
+screen.onkey(paddle.go_right, 'Right')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.xcor() > 280 or ball.xcor() < -280:
+        ball.bounce_x()
+    
+    # Detect collision with paddle
+    if ball.distance(paddle) < 30 and ball.ycor() > -300:
+        ball.bounce_y()
+        
+    if ball.ycor() < -300:
+        ball.reset_position()
+        scores.update_lives()
+        if scores.lives == 0:
+            scores.gameover()
+            game_is_on = False
+
+    # Detect collision with brick
+    for brick in bricks.all_bricks:
+        if ball.distance(brick) < 30:
+            ball.bounce_y()
+            brick.hideturtle()
+            bricks.all_bricks.remove(brick)
+            scores.update_points()
+
+
+screen.exitonclick()
