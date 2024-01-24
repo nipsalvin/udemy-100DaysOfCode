1cf40092232dd186eb1c0473fc42f547598e2a57
nipsalvin <amwaniki.am@gmail.com>
2024-01-24 07:09:49 +0300

    TTS using an API

--- day-91/tts.py
+++ day-91/tts.py
@@ -1,23 +1,15 @@
-import requests
+from pyht import Client
+from dotenv import load_dotenv
+from pyht.client import TTSOptions
 import os
+load_dotenv()
 
-url = "https://voicerss-text-to-speech.p.rapidapi.com/"
-
-querystring = {"key":"hello"}
-
-payload = {
-	"src": "Hello, world!",
-	"hl": "en-us",
-	"r": "0",
-	"c": "mp3",
-	"f": "8khz_8bit_mono"
-}
-headers = {
-	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Key": os.getenv('X_RapidAPI_Key'),
-	"X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com"
-}
-
-response = requests.post(url, data=payload, headers=headers, params=querystring)
-
-print(response.json())
+# FIXME: client not getting '_timer'
+client = Client(  
+   user_id=os.getenv('PLAY_HT_ID'),  
+   api_key=os.getenv('PLAY_HT_KEY'),  
+)
+VOICE_URI = "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json"
+for chunk in client.tts("Can you tell me your account email or, ah your phone number?", voice=VOICE_URI):
+    # do something with the audio chunk
+    print(type(chunk))
