c17f9c273a1034f3bbf7e8dc9a41701b7ef13471
nipsalvin <amwaniki.am@gmail.com>
2024-02-28 06:41:28 +0300

    updates on `Chrome dino` bot

--- day-94/main.py
+++ day-94/main.py
@@ -1,5 +1,6 @@
 from pyautogui import *
 import pyautogui
+import keyboard
 import time
 import random
 import win32api, win32con
@@ -8,6 +9,7 @@ time.sleep(1)
 jumps = 0
 
 def jump():
+    import ipdb;ipdb.set_trace()
     global jumps
     win32api.keybd_event(38, 0, 0, 0)
     time.sleep(0.01)
@@ -16,6 +18,7 @@ def jump():
 
 
 def duck():
+    import ipdb;ipdb.set_trace()
     win32api.keybd_event(40, 0, 0, 0)
     time.sleep(0.4)
     win32api.keybd_event(40, 0, win32con.KEYEVENTF_KEYUP, 0)
@@ -24,10 +27,11 @@ def duck():
 
 # Press 'q' to stop running
 while not keyboard.is_pressed('q'):
-    if pyautogui.pixel(730, 468)[0] == 83:
+    import ipdb;ipdb.set_trace()
+    if pyautogui.pixel(250, 540)[0] == 83:
         jump()
         print("I see a tall cactus!")
-    elif pyautogui.pixel(715, 490)[0] == 83:
+    elif pyautogui.pixel(250, 540)[0] == 83:
         jump()
         print("I see a short cactus!")
     if pyautogui.pixel(760, 460)[0] == 83:
