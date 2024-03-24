7045615b237dfa8879308d8df17f54370c4988e3
nipsalvin <amwaniki.am@gmail.com>
2024-03-24 19:31:44 +0300

    day-97 progress

--- day-97/forms.py
+++ day-97/forms.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from flask_wtf.file import FileAllowed, FileField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 # Create a form to register new users
 class RegisterForm(FlaskForm):
+    first_name = StringField("First Name", validators=[DataRequired()])
+    surname = StringField("Surname", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign Me Up!")
 
 
@@ -16,3 +18,13 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Let Me In!")
+
+class AddProductForm(FlaskForm):
+    scent_code = StringField(label='Scent Code', validators=[DataRequired()])
+    scent_name = StringField(label='Scent Name', validators=[DataRequired()])
+    scent_brand = StringField(label='Brand', validators=[DataRequired()])
+    scent_gender = StringField(label='Gender', validators=[DataRequired()])
+    grammage = StringField(label='Grammage', validators=[DataRequired()])
+    is_discounted = BooleanField(label='Is Discounted', default=False)
+    image = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    submin = SubmitField('Submit Product')
--- day-97/main.py
+++ day-97/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask import Flask, abort, render_template, redirect, url_for, flash, request, current_app
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
@@ -8,14 +8,16 @@ from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
-from forms import RegisterForm, LoginForm #, CommentForm
+from forms import RegisterForm, LoginForm, AddProductForm #, CommentForm
 import os
 import smtplib
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+app.config['UPLOAD_FOLDER'] = '/home/nips/Projects/udemy-100DaysOfCode/day-97/static/assets/img'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
+
 
 # Configure Flask-Login
 login_manager = LoginManager()
@@ -23,43 +25,75 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return db.get_or_404(User, user_id)
+    return db.session.get(User, user_id)
 
 
 # # For adding profile images to the comment section
-# gravatar = Gravatar(app,
-#                     size=100,
-#                     rating='g',
-#                     default='retro',
-#                     force_default=False,
-#                     force_lower=False,
-#                     use_ssl=False,
-#                     base_url=None)
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
 
 # # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI", "sqlite:///e_commerce.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///e_commerce.db"
 db = SQLAlchemy()
 db.init_app(app)
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    perfume_id = db.Column(db.Integer, db.ForeignKey('perfumes.id'))
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))
+    surname = db.Column(db.String(100))
+    likes = db.relationship('Like', backref='user', lazy=True)
 
+    def __repr__(self):
+        return f"<User: {self.first_name}>"
 
-# # Create an admin-only decorator
-# def admin_only(f):
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         # If id is not 1 then return abort with 403 error
-#         if current_user.id != 1:
-#             return abort(403)
-#         # Otherwise continue with the route function
-#         return f(*args, **kwargs)
+class Perfume(db.Model):
+    __tablename__ = 'perfumes'
+    id = db.Column(db.Integer, primary_key=True)
+    scent_code = db.Column(db.String(5), nullable=True, unique=True)
+    scent_name = db.Column(db.String(50))
+    scent_brand = db.Column(db.String(50))
+    scent_gender = db.Column(db.String(10)) 
+    grammage = db.Column(db.Integer)
+    is_discounted = db.Column(db.Boolean, default=False)
+    is_out = db.Column(db.Boolean, default=False)
+    image = db.Column(db.String(255), nullable=True)
+    price = db.Column(db.Integer)
+    description = db.Column(db.String(200))
+    likes = db.relationship('Like', backref='perfume', lazy=True)
 
-    # return decorated_function
+    def __repr__(self):
+        return f"<Perfume: {self.scent_code} {self.scent_name}>"
+
+    
+with app.app_context():
+    db.create_all()
+
+# Create an admin-only decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If id is not 1 then return abort with 403 error
+        if current_user.id != 1:
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+
+    return decorated_function
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
@@ -81,14 +115,15 @@ def register():
         )
         new_user = User(
             email=form.email.data,
-            name=form.name.data,
+            first_name=form.first_name.data,
+            surname=form.surname.data,
             password=hash_and_salted_password,
         )
         db.session.add(new_user)
         db.session.commit()
         # This line will authenticate the user with Flask-Login
         login_user(new_user)
-        return redirect(url_for("get_all_posts"))
+        return redirect(url_for("home"))
     return render_template("register.html", form=form, current_user=current_user)
 
 @app.route('/login', methods=["GET", "POST"])
@@ -109,7 +144,7 @@ def login():
             return redirect(url_for('login'))
         else:
             login_user(user)
-            return redirect(url_for('get_all_posts'))
+            return redirect(url_for('home'))
 
     return render_template("login.html", form=form, current_user=current_user)
 
@@ -126,7 +161,68 @@ def home():
 def view_cart():
     return render_template('cart.html')
 
+@app.route('/like/<int:perfume_id>', methods=["POST"])
+def toggle_like_perfume(perfume_id):
+    if current_user.is_authenticated:
+        like = Like.query.filter_by(user_id=current_user.id, perfume_id=perfume_id).first()
+        if like:
+            db.session.delete(like)
+            db.session.commit()
+            flash('Perfume unliked successfully!')
+        else:
+            like = Like(user_id=current_user.id, perfume_id=perfume_id)
+            db.session.add(like)
+            db.session.commit()
+            flash('Perfume liked successfully!')
+    else:
+        flash('Please log in to like/unlike perfumes.')
+    return redirect(url_for('home'))
 
+def save_image(image):
+    if image:
+        filename = image.filename
+        image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        image.save(image_path)
+        return filename
+    else:
+        return None
+
+@app.route('/add_product', methods=['GET', 'POST'])
+@admin_only
+def add_product():
+    form = AddProductForm()
+    if form.validate_on_submit():
+        # Handle file upload and store image filename in the database
+        image_filename = None
+        if form.image.data:
+            image_filename = save_image(form.image.data)  # Function to save image file and return filename
+        # Create a new Perfume object
+        new_perfume = Perfume(
+            scent_code=form.scent_code.data,
+            scent_name=form.scent_name.data,
+            scent_brand=form.scent_brand.data,
+            scent_gender=form.scent_gender.data,
+            grammage=form.grammage.data,
+            is_discounted=form.is_discounted.data,
+            image=image_filename  # Store the image filename in the Perfume object
+        )
+        # Add the new Perfume object to the database
+        db.session.add(new_perfume)
+        db.session.commit()
+        flash(f'{new_perfume.scent_name} added successfully!', 'success')
+        return redirect(url_for('home'))
+    return render_template('add_product.html', form=form, current_user=current_user)
+
+@app.route('/product/<int:perfume_id>', methods=['GET','POST'])
+def view_perfume(perfume_id):
+    requested_perfume = db.get_or_404(Perfume, perfume_id)
+    return render_template('details.html', product=requested_perfume, current_user=current_user)
+
+@app.route('/products')
+def get_all_products():
+    result = db.session.execute(db.select(Perfume))
+    products = result.scalars().all()
+    return render_template('products.html', products=products, current_user=current_user)
 
 if __name__ == "__main__":
     app.run(debug=True)
